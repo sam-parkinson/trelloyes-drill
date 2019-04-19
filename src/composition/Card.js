831b00581b6e3d59fba7383998c0f9404fa83a20
Sam Parkinson <parkinsonsp42@gmail.com>
2019-04-19 11:44:48 -0400

    Update card

--- src/composition/Card.js
+++ src/composition/Card.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Card.css'
 
 function Card(props) {
-  console.log(props);
-  return null;
+  return (
+    <div className='Card' key={props.id}>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  );
 }
 
 export default Card;
